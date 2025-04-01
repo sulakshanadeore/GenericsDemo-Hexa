0061b59c634b64f9909fc9a873103c9cac38776d
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-01 12:01:00 +0530

    Add files via upload

---
+++ Orders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoWithGenerics
+{
+    public class Orders
+    {
+        public int OrderID { get; set; }
+        public DateOnly OrderDate { get; set; }
+
+        public Customer CustomerDetails { get; set; }
+
+        public List<Products> MyCart { get; set; }
+    }
+}
---
+++ Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoWithGenerics
+{
+    public class Products
+    {
+        public int ProductID { get; set; }
+        public string ProdName { get; set; }
+        public int Price { get; set; }
+        public string UnitOfMeasurement { get; set; }
+        public int Qty { get; set; }
+    }
+}
