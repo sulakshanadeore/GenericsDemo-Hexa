383ef058a5f9273907f0d79e41bc107457f5ae88
sulakshanadeore <36725147+sulakshanadeore@users.noreply.github.com>
2025-04-01 13:16:48 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoWithGenerics
+{
+    public class Employee:IComparable<Employee>
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+        public DateTime JoiningDate { get; set; }
+
+        public int CompareTo(Employee? other)
+        {
+            return this.JoiningDate.CompareTo(other.JoiningDate);
+        }
+    }
+}
---
+++ Jewellery.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDemoWithGenerics
+{
+    public class Jewellery
+    {
+        public int OrnamentID { get; set; }
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public string MetalName { get; set; }
+    }
+
+
+    public class JewelleryDetails
+    {
+        public List<Jewellery> Jewelleries { get; set; }
+
+
+    }
+}
